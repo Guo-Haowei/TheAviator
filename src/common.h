074af563c343610adc90c6aefe19fef86b526eaf
Guo-Haowei <ghwhotline@gmail.com>
2017-12-31 18:17:02 -0500

    First commit

---
+++ src/common.h
@@ -0,0 +1,12 @@
+#pragma once
+// the color palette
+const float RED[] = { 242/255, 83/255, 70/255 };
+const float WHITE[] = { 216/255, 208/255, 209/255 };
+const float BROWN[] = { 89/255, 51/255, 46/255 };
+const float PINK[] = { 245/255, 152/255, 110/255 };
+const float BROWNDARK[] = { 35/255, 25/255, 15/255 };
+const float BLUE[] = { 104/255, 195/255, 192/255 };
+
+extern int WINDOW_WIDTH, WINDOW_HEIGHT;
+
+const float NEAR_PLANE = 0.1f, FAR_PLANE = 1000.0f, ZOOM = 45.0f;
