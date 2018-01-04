2b811e0de4a9daa5fd314361fcffef6c94150f64
Guo-Haowei <ghwhotline@gmail.com>
2018-01-03 22:29:11 -0500

    Camera

---
+++ src/entities/Camera.cc
@@ -0,0 +1,34 @@
+// Camera.cc
+#include "Camera.h"
+#include <glm/gtc/matrix_transform.hpp>
+
+Camera::Camera() {
+  position = glm::vec3(0.0f, 0.0f, 10.0f);
+  up = glm::vec3(0.0f, 1.0f, 0.0f);
+  worldUp = up;
+  front = glm::vec3(0.0f, 0.0f, -1.0f);
+  yaw = YAW;
+  pitch = PITCH;
+  zoom = ZOOM;
+  updateCameraVectors();
+}
+
+void Camera::update() {
+}
+
+glm::mat4 Camera::getViewMatrix() {
+  return glm::lookAt(position, position + front, up);
+}
+
+void Camera::updateCameraVectors() {
+  // calculate updated front vector
+  glm::vec3 updatedFront;
+  updatedFront.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+  updatedFront.y = sin(glm::radians(pitch));
+  updatedFront.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+  front = glm::normalize(updatedFront);
+  // update right and up vector
+  right = glm::normalize(glm::cross(front, worldUp));
+  up = glm::normalize(glm::cross(right, front));
+}
+
---
+++ src/entities/Camera.h
@@ -0,0 +1,27 @@
+// Camera.h
+#pragma once
+#include <glm/glm.hpp>
+
+const float YAW = -90.0f;
+const float PITCH = 0.0f;
+const float SPEED = 1.0f;
+const float ZOOM = 45.0f;
+
+class Camera {
+private:
+  glm::vec3 position;
+  glm::vec3 front;
+  glm::vec3 up;
+  glm::vec3 right;
+  glm::vec3 worldUp;
+
+  float yaw, pitch, zoom;
+
+  void updateCameraVectors();
+public:
+  Camera();
+
+  void update();
+  glm::mat4 getViewMatrix();
+  float getZoom() const;
+};
