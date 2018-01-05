fd2457a414284b6b9310bfbf0cccc62a3229443d
Guo-Haowei <ghwhotline@gmail.com>
2018-01-05 17:08:17 -0500

    Add sin, cos and rand

--- src/utils/Maths.h
+++ src/utils/Maths.h
@@ -4,4 +4,8 @@
 namespace Maths {
   extern float PI;
   float toRadians(float degree);
+  float sin(float degree);
+  float cos(float degree);
+  float rand();
+  float rand(float min, float max);
 };
