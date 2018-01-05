a19bf0fa57d9263a95f2bc6d51d3f4e0edc79379
Guo-Haowei <ghwhotline@gmail.com>
2018-01-04 22:08:41 -0500

    Add Debug methods

---
+++ src/utils/Debug.cc
@@ -0,0 +1,15 @@
+// Debug.cc
+#include "Debug.h"
+#include <iostream>
+using std::cerr;
+
+void Debug::printVector(glm::vec3 vec) {
+  cerr << "[" << vec.x << " " << vec.y << " " << vec.z << "]\n";
+}
+
+void Debug::printMatrix(glm::mat4 mat) {
+  cerr << "[" << mat[0].x << " " << mat[1].x << " " << mat[2].x << " " << mat[3].x << "]\n";
+  cerr << "[" << mat[0].y << " " << mat[1].y << " " << mat[2].y << " " << mat[3].y << "]\n";
+  cerr << "[" << mat[0].z << " " << mat[1].z << " " << mat[2].z << " " << mat[3].z << "]\n";
+  cerr << "[" << mat[0].w << " " << mat[1].w << " " << mat[2].w << " " << mat[3].w << "]\n";
+}
---
+++ src/utils/Debug.h
@@ -0,0 +1,8 @@
+// Debug.h
+#pragma once
+#include <glm/glm.hpp>
+
+namespace Debug {
+  void printVector(glm::vec3 vec);
+  void printMatrix(glm::mat4 mat);
+};
