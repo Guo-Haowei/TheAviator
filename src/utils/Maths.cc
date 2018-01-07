2fdbb74aa6a7640626cf28c640bdea52181269b8
Guo-Haowei <ghwhotline@gmail.com>
2018-01-07 12:03:09 -0500

    Fix Maths functions

--- src/utils/Maths.cc
+++ src/utils/Maths.cc
@@ -1,26 +1,13 @@
 // Maths.cc
 #include "Maths.h"
 #include <cstdlib>
-#include <math.h>
-
-float Maths::PI = 3.141593;
+#include <iostream>
+using std::cout;
 
 float Maths::toRadians(float degree) {
   return degree * PI / 180.0f;
 }
 
-float Maths::sin(float degree) {
-  return sin(toRadians(degree));
-}
-
-float Maths::cos(float degree) {
-  return cos(toRadians(degree));
-}
-
-float Maths::rand() {
-  return (float)rand() / (float)RAND_MAX;
-}
-
 float Maths::rand(float min, float max) {
-  return min + rand() * (max - min);
+  return min + (float)std::rand() / (float)RAND_MAX * (max - min);
 }
--- src/utils/Maths.h
+++ src/utils/Maths.h
@@ -1,11 +1,8 @@
 // Maths.h
 #pragma once
+#define PI 3.14159265358979323846
 
 namespace Maths {
-  extern float PI;
   float toRadians(float degree);
-  float sin(float degree);
-  float cos(float degree);
-  float rand();
   float rand(float min, float max);
 };
