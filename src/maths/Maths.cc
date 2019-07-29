f7c1283e5a6810134db1da297124d75302c66bd8
Guo-Haowei <ghwhotline@gmail.com>
2019-07-29 00:22:10 -0700

    Fix bugs caused by glm api change

--- src/maths/Maths.cc
+++ src/maths/Maths.cc
@@ -30,13 +30,13 @@ bool Maths::chance(float chance) {
 
 glm::mat4 Maths::calculateTranslationMatrix(float x, float y, float z) {
   glm::vec3 delta = glm::vec3(x, y, z);
-  glm::mat4 translation(1);
+  glm::mat4 translation(1.0f);
   translation = glm::translate(translation, delta);
   return translation;
 }
 
 glm::mat4 Maths::calculateRotationMatrix(float x, float y, float z, glm::vec3 center) {
-  glm::mat4 rotationMatrix, T, T_1;
+  glm::mat4 rotationMatrix(1.0f), T(1.0f), T_1(1.0f);
   T = glm::translate(T, -center);
   T_1 = glm::translate(T_1, center);
 
@@ -49,30 +49,11 @@ glm::mat4 Maths::calculateRotationMatrix(float x, float y, float z, glm::vec3 ce
 }
 
 glm::mat4 Maths::rotateAroundAxis(glm::vec3 axis, float angle, glm::vec3 center) {
-  glm::mat4 rotationMatrix, T, T_1;
+  glm::mat4 rotationMatrix(1.0f), T(1.0f), T_1(1.0f);
   T = glm::translate(T, -center);
   T_1 = glm::translate(T_1, center);
 
-  axis = glm::normalize(axis);
-  float yz = sqrt(axis.y * axis.y + axis.z * axis.z);
-  float xz = sqrt(axis.x * axis.x + axis.z * axis.z);
-  if (yz != 0.0f) {
-    glm::mat4 Rx = glm::mat4(1.0f), RxInverse = glm::mat4(1.0f), Ry = glm::mat4(1.0f), RyInverse = glm::mat4(1.0f), Rz = glm::mat4(1.0f);
-    float sinTheta1 = axis.y / yz, cosTheta1 = axis.z / yz;
-    Rx[1].y = cosTheta1; Rx[1].z = sinTheta1; Rx[2].y = -sinTheta1; Rx[2].z = cosTheta1;
-    RxInverse[1].y = cosTheta1; RxInverse[1].z = -sinTheta1; RxInverse[2].y = sinTheta1; RxInverse[2].z = cosTheta1;
-
-    float sinTheta2 = -axis.x, cosTheta2 = yz;
-    Ry[0].x = cosTheta2; Ry[0].z = -sinTheta2; Ry[2].x = sinTheta2; Ry[2].z = cosTheta2;
-    RyInverse[0].x = cosTheta2; RyInverse[0].z = sinTheta2; RyInverse[2].x = -sinTheta2; RyInverse[2].z = cosTheta2;
-
-    float cosTheta = glm::cos(angle), sinTheta = glm::sin(angle);
-    Rz[0].x = cosTheta; Rz[0].y = sinTheta; Rz[1].x = -sinTheta; Rz[1].y = cosTheta;
-
-    rotationMatrix = RxInverse * RyInverse * Rz * Ry * Rx;
-  } else {
-    rotationMatrix = glm::rotate(rotationMatrix, angle, axis);
-  }
+  rotationMatrix = glm::rotate(rotationMatrix, angle, glm::normalize(axis));
 
   return T_1 * rotationMatrix * T;
 }
